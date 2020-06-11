ac73399b8026484b291022f5c7cb9af087927453
rakeshcheekatimala <sri.rakesh327@gmail.com>
2020-06-12 00:53:16 +0800

    Added style to sticky navigation and animations to sticky navigation, navigation, buttons, works, cities and plans using Jquery plugin (waypoint).

---
+++ Resources/JS/script.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+
+    /*------------Sticky Navigation----------------*/
+    
+        $('.js-section-features').waypoint(function(direction){
+            if (direction == "down")
+            {
+                $('nav').addClass('sticky');
+            }
+            else
+            {
+                $('nav').removeClass('sticky');
+            }
+        },  {
+            offset: '60px'
+            }
+         );
+    
+    /*---------------Scroll on Buttons----------------*/
+    
+         $('.js-scroll-to-plan').click(function(){
+    
+            $('html,body').animate({scrollTop:$('.js-section-plans').offset().top},1000);
+    
+         });
+    
+         $('.js-scroll-to-start').click(function(){
+    
+            $('html,body').animate({scrollTop:$('.js-section-features').offset().top},1000);
+    
+         });
+    
+    
+    /*---------------Navigation Scroll-----------------*/
+    
+        $(function(){
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+                || location.hostname == this.hostname) {
+        
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                   if (target.length) {
+                     $('html,body').animate({
+                         scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    
+    /*----------------Animation on scrolls----------------*/
+    
+    $('.js-row').waypoint(function(direction){
+        $('.js-row').addClass('animated fadeIn');
+    
+    },
+       {
+        offset:'50%'
+       }
+    );
+    
+    
+    $(".js-works").waypoint(function(direction){
+        $(".js-works").addClass("animated fadeInUp");
+    
+    },{
+        offset:"50%"
+      }
+    );
+    
+    $(".js-cities").waypoint(function(direction){
+        $(".js-cities").addClass("animated fadeInLeft");
+    
+    },{
+        offset:"50%"
+      }
+    );
+    
+    $(".js-cities-1").waypoint(function(direction){
+        $(".js-cities-1").addClass("animated fadeInRight");
+    
+    },{
+        offset:"50%"
+      }
+    );
+    
+    $(".js-plans").waypoint(function(direction){
+        $(".js-plans").addClass("animated pulse");
+    
+    },{
+        offset:"50%"
+      }
+    );
+    
+    });
+    
+    
+    
+    
+    
+        
+    
+    
+    
+    
+        
